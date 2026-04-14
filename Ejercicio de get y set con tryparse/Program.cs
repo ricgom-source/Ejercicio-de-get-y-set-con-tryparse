40537f590505b3d77dc41ee5988c1e3542c3aea1
ricogm-source <ricardoggmenchu@gmail.com>
2026-04-14 08:57:07 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio de get y set con tryparse/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_get_y_set_con_tryparse
+{
+    internal class Program
+
+    {
+        class Estudiante
+
+        {
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
